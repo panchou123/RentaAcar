7eec3f49e39ccc9562ba3ba69cdae24a185d0908
FRANCISCO RAMON DELGADO PEREZ <fdelgado@uadec.edu.mx>
2023-11-27 22:17:00 -0600

    autos

---
+++ Autos.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Autos
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Autos))
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.txtdni = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txtapellidos = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.txtnombre = New System.Windows.Forms.TextBox()
+        Me.txtcodigo = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btncancelar = New System.Windows.Forms.Button()
+        Me.btnmodificar = New System.Windows.Forms.Button()
+        Me.btneliminar = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.txtbuscar = New System.Windows.Forms.TextBox()
+        Me.dgvautos = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvautos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Enabled = False
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(61, 56)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(46, 54)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 151
+        Me.PictureBox4.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.ForeColor = System.Drawing.Color.Transparent
+        Me.Button2.Location = New System.Drawing.Point(61, 67)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(34, 33)
+        Me.Button2.TabIndex = 150
+        Me.Button2.Text = "Nuevo"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Enabled = False
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(239, 54)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(46, 54)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 149
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Enabled = False
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(144, 56)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(46, 54)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 148
+        Me.PictureBox2.TabStop = False
+        '
+        'txtdni
+        '
+        Me.txtdni.Enabled = False
+        Me.txtdni.Location = New System.Drawing.Point(171, 253)
+        Me.txtdni.MaxLength = 10
+        Me.txtdni.Name = "txtdni"
+        Me.txtdni.Size = New System.Drawing.Size(143, 20)
+        Me.txtdni.TabIndex = 147
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(81, 257)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(0, 16)
+        Me.Label11.TabIndex = 146
+        '
+        'txtapellidos
+        '
+        Me.txtapellidos.Enabled = False
+        Me.txtapellidos.Location = New System.Drawing.Point(171, 215)
+        Me.txtapellidos.MaxLength = 50
+        Me.txtapellidos.Name = "txtapellidos"
+        Me.txtapellidos.Size = New System.Drawing.Size(143, 20)
+        Me.txtapellidos.TabIndex = 145
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(105, 219)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(53, 16)
+        Me.Label12.TabIndex = 144
+        Me.Label12.Text = "Modelo"
+        '
+        'txtnombre
+        '
+        Me.txtnombre.Enabled = False
+        Me.txtnombre.Location = New System.Drawing.Point(171, 174)
+        Me.txtnombre.MaxLength = 50
+        Me.txtnombre.Name = "txtnombre"
+        Me.txtnombre.Size = New System.Drawing.Size(143, 20)
+        Me.txtnombre.TabIndex = 143
+        '
+        'txtcodigo
+        '
+        Me.txtcodigo.Enabled = False
+        Me.txtcodigo.Location = New System.Drawing.Point(171, 140)
+        Me.txtcodigo.MaxLength = 10
+        Me.txtcodigo.Name = "txtcodigo"
+        Me.txtcodigo.Size = New System.Drawing.Size(143, 20)
+        Me.txtcodigo.TabIndex = 142
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(105, 175)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(46, 16)
+        Me.Label13.TabIndex = 141
+        Me.Label13.Text = "Precio"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.Location = New System.Drawing.Point(70, 144)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(81, 16)
+        Me.Label14.TabIndex = 140
+        Me.Label14.Text = "Codigo Auto"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(51, 315)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(88, 42)
+        Me.Button1.TabIndex = 138
+        Me.Button1.Text = "Guardar"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'btncancelar
+        '
+        Me.btncancelar.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btncancelar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btncancelar.Location = New System.Drawing.Point(171, 315)
+        Me.btncancelar.Name = "btncancelar"
+        Me.btncancelar.Size = New System.Drawing.Size(85, 42)
+        Me.btncancelar.TabIndex = 139
+        Me.btncancelar.Text = "Cancelar"
+        Me.btncancelar.UseVisualStyleBackColor = False
+        '
+        'btnmodificar
+        '
+        Me.btnmodificar.BackColor = System.Drawing.Color.Transparent
+        Me.btnmodificar.ForeColor = System.Drawing.Color.Transparent
+        Me.btnmodificar.Location = New System.Drawing.Point(160, 65)
+        Me.btnmodificar.Name = "btnmodificar"
+        Me.btnmodificar.Size = New System.Drawing.Size(30, 33)
+        Me.btnmodificar.TabIndex = 136
+        Me.btnmodificar.Text = "Modificar"
+        Me.btnmodificar.UseVisualStyleBackColor = False
+        '
+        'btneliminar
+        '
+        Me.btneliminar.BackColor = System.Drawing.Color.Transparent
+        Me.btneliminar.ForeColor = System.Drawing.Color.Transparent
+        Me.btneliminar.Location = New System.Drawing.Point(256, 65)
+        Me.btneliminar.Name = "btneliminar"
+        Me.btneliminar.Size = New System.Drawing.Size(31, 33)
+        Me.btneliminar.TabIndex = 137
+        Me.btneliminar.Text = "Eliminar"
+        Me.btneliminar.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PictureBox5)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.txtbuscar)
+        Me.GroupBox1.Controls.Add(Me.dgvautos)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(325, 42)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(479, 366)
+        Me.GroupBox1.TabIndex = 135
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Lista de Clientes"
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Enabled = False
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(232, 30)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(39, 31)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 25
+        Me.PictureBox5.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(232, 31)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(35, 30)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Buscar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'txtbuscar
+        '
+        Me.txtbuscar.Location = New System.Drawing.Point(32, 30)
+        Me.txtbuscar.Multiline = True
+        Me.txtbuscar.Name = "txtbuscar"
+        Me.txtbuscar.Size = New System.Drawing.Size(194, 31)
+        Me.txtbuscar.TabIndex = 2
+        '
+        'dgvautos
+        '
+        Me.dgvautos.AllowUserToAddRows = False
+        Me.dgvautos.AllowUserToDeleteRows = False
+        Me.dgvautos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvautos.Location = New System.Drawing.Point(6, 76)
+        Me.dgvautos.Name = "dgvautos"
+        Me.dgvautos.ReadOnly = True
+        Me.dgvautos.Size = New System.Drawing.Size(456, 269)
+        Me.dgvautos.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(81, 257)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 16)
+        Me.Label1.TabIndex = 152
+        Me.Label1.Text = "Kilometraje"
+        '
+        'Autos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(832, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.txtdni)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.txtapellidos)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.txtnombre)
+        Me.Controls.Add(Me.txtcodigo)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btncancelar)
+        Me.Controls.Add(Me.btnmodificar)
+        Me.Controls.Add(Me.btneliminar)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Autos"
+        Me.Text = "Autos"
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvautos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents txtdni As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents txtapellidos As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents txtnombre As TextBox
+    Friend WithEvents txtcodigo As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btncancelar As Button
+    Friend WithEvents btnmodificar As Button
+    Friend WithEvents btneliminar As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents txtbuscar As TextBox
+    Friend WithEvents dgvautos As DataGridView
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Autos.vb
@@ -0,0 +1,110 @@
+﻿Public Class Autos
+    Dim dtautos As DataTable
+
+    Private Sub FormAutos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        bloqueacajas(False)
+        mostrar_autos()
+        limpiar()
+    End Sub
+
+    Public Sub mostrar_autos()
+        Dim c As New FAutos
+        dtautos = c.mostrar_autos
+        dgvautos.DataSource = dtautos
+    End Sub
+
+    Sub nuevoAuto()
+        btncancelarAuto.Enabled = True
+        btnguardarAuto.Enabled = True
+        limpiar()
+        bloqueacajasAuto(True)
+    End Sub
+
+    Sub limpiarAuto()
+        txtCodigoAuto.Clear()
+        txtMarca.Clear()
+        txtModelo.Clear()
+        txtAnio.Clear()
+    End Sub
+
+    Sub cancelarAuto()
+        btnNuevoAuto.Enabled = True
+        btnguardarAuto.Enabled = False
+        btnEliminarAuto.Enabled = False
+        btncancelarAuto.Enabled = False
+        btnModificarAuto.Enabled = False
+        limpiarAuto()
+        bloqueacajasAuto(False)
+    End Sub
+
+    Sub bloqueacajasAuto(va As Boolean)
+        txtMarca.Enabled = va
+        txtModelo.Enabled = va
+        txtAnio.Enabled = va
+    End Sub
+
+    Private Sub btnNuevoAuto_Click(sender As Object, e As EventArgs) Handles btnNuevoAuto.Click
+        nuevoAuto()
+    End Sub
+
+    Private Sub btncancelarAuto_Click(sender As Object, e As EventArgs) Handles btncancelarAuto.Click
+        cancelarAuto()
+    End Sub
+
+    Private Sub btnguardarAuto_Click(sender As Object, e As EventArgs) Handles btnguardarAuto.Click
+        Dim c As New FAutos
+        If (txtMarca.Text <> String.Empty And txtModelo.Text <> String.Empty And txtAnio.Text <> String.Empty) Then
+            Try
+                If (txtCodigoAuto.Text <> String.Empty) Then
+                    If (c.editarAuto(txtCodigoAuto.Text, txtMarca.Text, txtModelo.Text, txtAnio.Text) = True) Then
+                        MessageBox.Show("Auto editado correctamente", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        cancelarAuto()
+                        mostrar_autos()
+                    Else
+                        MessageBox.Show("Error al modificar el Auto")
+                    End If
+                Else
+                    If (c.insertarAuto(txtCodigoAuto.Text, txtMarca.Text, txtModelo.Text, txtAño.Text) = True) Then
+                        MessageBox.Show("Auto registrado correctamente", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        cancelarAuto()
+                        mostrar_autos()
+                    Else
+                        MessageBox.Show("Error al registrar el Auto")
+                    End If
+                End If
+            Catch ex As Exception
+                MessageBox.Show("Verifique los datos ingresados", "Datos", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End Try
+        Else
+            MessageBox.Show("Complete todos los datos")
+        End If
+    End Sub
+
+    Private Sub dgvautos_DoubleClick(sender As Object, e As EventArgs) Handles dgvautos.DoubleClick
+        txtCodigoAuto.Text = dgvautos.CurrentRow.Cells("codigo_auto").Value
+        txtMarca.Text = dgvautos.CurrentRow.Cells("marca").Value
+        txtModelo.Text = dgvautos.CurrentRow.Cells("modelo").Value
+        txtAnio.Text = dgvautos.CurrentRow.Cells("anio").Value
+
+        btnguardarAuto.Enabled = False
+        btnEliminarAuto.Enabled = True
+        bloqueacajasAuto(True)
+    End Sub
+
+    Private Sub btnModificarAuto_Click(sender As Object, e As EventArgs) Handles btnModificarAuto.Click
+        btnguardarAuto.Enabled = True
+        btnEliminarAuto.Enabled = False
+        bloqueacajasAuto(True)
+    End Sub
+
+    Private Sub btnEliminarAuto_Click(sender As Object, e As EventArgs) Handles btnEliminarAuto.Click
+        Dim c As New FAutos
+        If (c.eliminarAuto(txtCodigoAuto.Text) = True) Then
+            MessageBox.Show("Auto eliminado correctamente", "Eliminación", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            cancelarAuto()
+            mostrar_autos()
+        Else
+            MessageBox.Show("Error al eliminar el Auto")
+        End If
+    End Sub
+End Class
